ba49227c5bb4d79b8eb6e1443b2069c1530cb8f9
NouranAloui <120125352+NouranAloui@users.noreply.github.com>
2025-04-12 23:23:10 +0200

    edit

--- FireBaseConfig.js
+++ FireBaseConfig.js
@@ -1,13 +1,15 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import 'dotenv/config';
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyA_TrhK3kq1CeR1h3GCc1Jsuf1gfSiIQbA",
+    apiKey: process.env.FIREBASE_API_KEY,
     authDomain: "movie-87d0e.firebaseapp.com",
     projectId: "movie-87d0e",
     storageBucket: "movie-87d0e.appspot.com",
